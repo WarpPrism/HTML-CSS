b0449b07b171461fb0ba82534ec8c4b7dd63af1f
ZhouJihao <249662605@qq.com>
2015-04-20 23:34:03 +0800

    update3

--- Page0/page0.js
+++ Page0/page0.js
@@ -6,96 +6,96 @@ var headdiv;
 var curtaintime = 3000;
 
 window.onload = function() {
-	init();
-	openCurtain();
-	startgiftAnimation();
-	startRibbonAnimation();
-	setTimeout(showTip, 8000 + curtaintime);
-	setTimeout(showHead, 9000 + curtaintime);
+    init();
+    openCurtain();
+    startgiftAnimation();
+    startRibbonAnimation();
+    setTimeout(showTip, 8000 + curtaintime);
+    setTimeout(showHead, 9000 + curtaintime);
 }
 
 function init() {
-	curtain = document.getElementById("curtain");
-	gift = document.getElementById("gift");
-	tipdiv = document.getElementById("tipdiv");
-	ribbon = document.getElementById("ribbon");
-	headdiv = document.getElementById("headdiv");
+    curtain = document.getElementById("curtain");
+    gift = document.getElementById("gift");
+    tipdiv = document.getElementById("tipdiv");
+    ribbon = document.getElementById("ribbon");
+    headdiv = document.getElementById("headdiv");
 }
 
 function openCurtain() {
-	curtain.style.height = "0px";
-	curtain.style.background = "lightblue";
-	curtain.style.opacity = "0";
+    curtain.style.height = "0px";
+    curtain.style.background = "lightblue";
+    curtain.style.opacity = "0";
 }
 
 function startgiftAnimation() {
-	function animation0() {
-		gift.style.top = "400px";
-		gift.style.left = "200px";
-		gift.style.opacity = "1";
-	}
+    function animation0() {
+        gift.style.top = "400px";
+        gift.style.left = "200px";
+        gift.style.opacity = "1";
+    }
 
-	function animation1() {
-		gift.style.top = "0px";
-		gift.style.left = "400px";
-		gift.style.opacity = "0.3";
-	}
+    function animation1() {
+        gift.style.top = "0px";
+        gift.style.left = "400px";
+        gift.style.opacity = "0.3";
+    }
 
-	function animation2() {
-		gift.style.top = "400px";
-		gift.style.left = "600px";
-		gift.style.opacity = "1";
-	}
+    function animation2() {
+        gift.style.top = "400px";
+        gift.style.left = "600px";
+        gift.style.opacity = "1";
+    }
 
-	function animation3() {
-		gift.style.top = "0px";
-		gift.style.left = "800px";
-		gift.style.opacity = "0.4";
-	}
+    function animation3() {
+        gift.style.top = "0px";
+        gift.style.left = "800px";
+        gift.style.opacity = "0.4";
+    }
 
-	function animation4() {
-		gift.style.top = "300px";
-		gift.style.left = "1000px";
-		gift.style.opacity = "0.8";
-	}
+    function animation4() {
+        gift.style.top = "300px";
+        gift.style.left = "1000px";
+        gift.style.opacity = "0.8";
+    }
 
-	function animation5() {
-		gift.style.top = "300px";
-		gift.style.left = "600px";
-		gift.style.opacity = "1";
-	}
-	setTimeout(animation0, 0 + curtaintime);
-	setTimeout(animation1, 1000 + curtaintime);
-	setTimeout(animation2, 2000 + curtaintime);
-	setTimeout(animation3, 3000 + curtaintime);
-	setTimeout(animation4, 4000 + curtaintime);
-	setTimeout(animation5, 5000 + curtaintime);
+    function animation5() {
+        gift.style.top = "300px";
+        gift.style.left = "600px";
+        gift.style.opacity = "1";
+    }
+    setTimeout(animation0, 0 + curtaintime);
+    setTimeout(animation1, 1000 + curtaintime);
+    setTimeout(animation2, 2000 + curtaintime);
+    setTimeout(animation3, 3000 + curtaintime);
+    setTimeout(animation4, 4000 + curtaintime);
+    setTimeout(animation5, 5000 + curtaintime);
 }
 
 function startRibbonAnimation() {
 
-	function animation6() {
-		ribbon.style.opacity = "0.5";
-		ribbon.style.left = "400px";
-		ribbon.style.transform = "rotate(180deg)";
-	}
+    function animation6() {
+        ribbon.style.opacity = "0.5";
+        ribbon.style.left = "400px";
+        ribbon.style.transform = "rotate(180deg)";
+    }
 
-	function animation7() {
-		ribbon.style.opacity = "1";
-		ribbon.style.left = "630px";
-		ribbon.style.top = "400px";
-		ribbon.style.transform = "rotate(360deg)";
-	}
+    function animation7() {
+        ribbon.style.opacity = "1";
+        ribbon.style.left = "630px";
+        ribbon.style.top = "400px";
+        ribbon.style.transform = "rotate(360deg)";
+    }
 
-	setTimeout(animation6, 6000 + curtaintime);
-	setTimeout(animation7, 7000 + curtaintime);
+    setTimeout(animation6, 6000 + curtaintime);
+    setTimeout(animation7, 7000 + curtaintime);
 }
 
 function showTip() {
-	tipdiv.style.opacity = "1";
-	tipdiv.style.top = "250px";
+    tipdiv.style.opacity = "1";
+    tipdiv.style.top = "250px";
 }
 
 function showHead() {
-	headdiv.style.opacity = "1";
+    headdiv.style.opacity = "1";
 }
--- Page1/page1.js
+++ Page1/page1.js
@@ -17,225 +17,226 @@ var wordsdiv;
 var foot;
 
 window.onload = function() {
-	init();
-	openCurtain();
-	setTimeout(disableCurtain, 5000);
-	setTimeout(startLissajousAnimation, 6000);
+    init();
+    openCurtain();
+    setTimeout(disableCurtain, 5000);
+    setTimeout(startLissajousAnimation, 6000);
 }
 
 function init() {
-	n = 1;
-	Fi = 0;
-	index = 0;
-	wrap = document.getElementById("wrap");
-	curtain1 = document.getElementById("curtain1");
-	curtain2 = document.getElementById("curtain2");
-	span1 = curtain1.getElementsByTagName("span");
-	span2 = curtain2.getElementsByTagName("span");
-	garden = document.getElementById("garden");
-	textdiv = document.getElementById("textdiv");
-	text1 = document.getElementById("text1");
-	text2 = document.getElementById("text2");
-	wordsdiv = document.getElementById("wordsdiv");
-	foot = document.getElementById("foot");
-	canvas = document.getElementById("canvas");
-	context = canvas.getContext("2d");
-	context.globalCompositeOperation = "lighter";
-	garden = new Garden(context, canvas);
-	setInterval(function() {
-		garden.render();
-	}, Garden.options.growSpeed);
+    n = 1;
+    Fi = 0;
+    index = 0;
+    wrap = document.getElementById("wrap");
+    curtain1 = document.getElementById("curtain1");
+    curtain2 = document.getElementById("curtain2");
+    span1 = curtain1.getElementsByTagName("span");
+    span2 = curtain2.getElementsByTagName("span");
+    garden = document.getElementById("garden");
+    textdiv = document.getElementById("textdiv");
+    text1 = document.getElementById("text1");
+    text2 = document.getElementById("text2");
+    wordsdiv = document.getElementById("wordsdiv");
+    foot = document.getElementById("foot");
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    context.globalCompositeOperation = "lighter";
+    garden = new Garden(context, canvas);
+    setInterval(function() {
+        garden.render();
+    }, Garden.options.growSpeed);
 }
 
 function openCurtain() {
-	span1[0].style.opacity = "1";
-	span1[0].style.top = "250px";
-	span1[0].style.transform = "rotate(360deg)";
+    span1[0].style.opacity = "1";
+    span1[0].style.top = "250px";
+    span1[0].style.transform = "rotate(360deg)";
 
-	span2[0].style.opacity = "1";
-	span2[0].style.top = "250px";
-	span2[0].style.transform = "rotate(360deg)";
+    span2[0].style.opacity = "1";
+    span2[0].style.top = "250px";
+    span2[0].style.transform = "rotate(360deg)";
 
-	curtain1.style.width = "0px";
-	curtain1.style.opacity = "0";
-	curtain1.style.background = "lightblue";
+    curtain1.style.width = "0px";
+    curtain1.style.opacity = "0";
+    curtain1.style.background = "lightblue";
 
-	curtain2.style.width = "0px";
-	curtain2.style.opacity = "0";
-	curtain2.style.background = "lightblue";
+    curtain2.style.width = "0px";
+    curtain2.style.opacity = "0";
+    curtain2.style.background = "lightblue";
 }
 
 function disableCurtain() {
-	wrap.removeChild(curtain1);
-	wrap.removeChild(curtain2);
-	text1.style.opacity = "1";
-	foot.style.opacity = "1";
+    wrap.removeChild(curtain1);
+    wrap.removeChild(curtain2);
+    text1.style.opacity = "1";
+    foot.style.opacity = "1";
 }
 
 //Heart curve
 function getHeartCurveCoordinate(s) {
-	var t = s / Math.PI;
-	var x = 15 * (16 * Math.pow(Math.sin(t), 3));
-	var y = -16 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
-	return new Array(270 + x, 225 + y);
+    var t = s / Math.PI;
+    var x = 15 * (16 * Math.pow(Math.sin(t), 3));
+    var y = -16 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+    return new Array(270 + x, 225 + y);
 }
 
 //利萨茹(Lissajous)曲线
 function getLissajousCurveCoordinate(s, n, Fi) {
-	var t = s;
-	var x = 200 * Math.sin(t);
-	var y = 200 * Math.sin(n * t + Fi);
-	return new Array(280 + x, 270 + y);
+    var t = s;
+    var x = 200 * Math.sin(t);
+    var y = 200 * Math.sin(n * t + Fi);
+    return new Array(280 + x, 270 + y);
 }
 
 // y = x^3
 /*function getCurveCoordinate(s) {
-	var x = s;
-	var y = -0.0001 * Math.pow(x, 3);
-	return new Array(300 + x, 300 + y);
+    var x = s;
+    var y = -0.0001 * Math.pow(x, 3);
+    return new Array(300 + x, 300 + y);
 }*/
 
 function startLissajousAnimation() {
-	index++;
-	var s = -100;
-	var b = new Array();
-	var a = setInterval(function() {
-		var h = getLissajousCurveCoordinate(s, n, Fi);
-		var e = true;
-		for (var f = 0; f < b.length; f++) {
-			var g = b[f];
-			var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
-			if (j < Garden.options.bloomRadius.max * 1.3) {
-				e = false;
-				break;
-			}
-		}
-		if (e) {
-			b.push(h);
-			garden.createRandomBloom(h[0], h[1])
-		}
-		if (s >= 50) {
-			clearInterval(a);
-			clearCanvas();
-			clearCanvas();
-			switch (index) {
-				//初始为线段 line
-				//圆 circle
-				case 1:
-					Fi = Math.PI / 2;
-					startLissajousAnimation();
-					textdiv.style.width = "270px";
-					changeText();
-					text2.innerHTML = "x = 200sin(t)<br />y = 200sin(t + PI / 2)<br />x^2 + y^2 = 200^2<br />Let's leading a ring-like life.";
-					break;
-				//椭圆
-				case 2:
-					Fi = Math.PI / 3;
-					startLissajousAnimation();
-					textdiv.style.width = "300px";
-					changeText();
-					text1.innerHTML = "x = 200sin(t)<br />y = 200sin(t + PI / 3)<br />Ellipse<br />You provide the orbit for my life.";
-					break;
-				//抛物线
-				case 3:
-					n = 2;
-					Fi = Math.PI / 2;
-					startLissajousAnimation();
-					textdiv.style.width = "320px";
-					changeText();
-					text2.innerHTML = "x = 200sin(t)<br />y = 200sin(2t + PI / 2)<br />Parabola<br />I start to rise when meeting you.";
-					break;
-				//执罗诺双纽线
-				case 4:
-					Fi = 0;
-					startLissajousAnimation();
-					textdiv.style.width = "230px";
-					changeText();
-					text1.innerHTML = "x = 200sin(t)<br />y = 200sin(2t)<br />Lemniscate<br />Combine together.";
-					break;
-				//besace曲线
-				case 5:
-					Fi = Math.PI / 3;
-					startLissajousAnimation();
-					textdiv.style.width = "220px";
-					changeText();
-					text2.innerHTML = "x = 200sin(t)<br />y = 200sin(2t)<br />Besace<br />Getting beautiful.";
-					break;
-				//心形线
-				case 6:
-					startHeartAnimation();
-					text2.style.display = "none";
-					textdiv.style.left = "730px";
-					textdiv.style.top = "190px";
-					textdiv.style.width = "370px";
-					textdiv.style.height = "100px";
-					textdiv.style.boxShadow = "0px 0px 0px #FFF";
-					text1.style.letterSpacing = "0px";
-					text1.style.textAlign = "center";
-					text1.style.opacity = "1";
-					textdiv.style.transition = "all 1.5s linear";
-					text1.style.transition = "all 2s linear";
-					text1.innerHTML = "x = 15 * 16[sin(t)]^3<br />y = -16 * (13cost - 5cos2t - 2cos3t - cos4t)<br />Heart<br />I am fond of you.";
-				default:
-					break;
-			}
-		} else {
-			s += 0.2
-		}
-	}, 10)
+    index++;
+    var s = -100;
+    var b = new Array();
+    var a = setInterval(function() {
+        var h = getLissajousCurveCoordinate(s, n, Fi);
+        var e = true;
+        for (var f = 0; f < b.length; f++) {
+            var g = b[f];
+            var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
+            if (j < Garden.options.bloomRadius.max * 1.3) {
+                e = false;
+                break;
+            }
+        }
+        if (e) {
+            b.push(h);
+            garden.createRandomBloom(h[0], h[1])
+        }
+        if (s >= 50) {
+            clearInterval(a);
+            clearCanvas();
+            clearCanvas();
+            switch (index) {
+                //初始为线段 line
+                //圆 circle
+                case 1:
+                    Fi = Math.PI / 2;
+                    startLissajousAnimation();
+                    textdiv.style.width = "270px";
+                    changeText();
+                    text2.innerHTML = "x = 200sin(t)<br />y = 200sin(t + PI / 2)<br />x^2 + y^2 = 200^2<br />Let's leading a ring-like life.";
+                    break;
+                //椭圆
+                case 2:
+                    Fi = Math.PI / 3;
+                    startLissajousAnimation();
+                    textdiv.style.width = "300px";
+                    changeText();
+                    text1.innerHTML = "x = 200sin(t)<br />y = 200sin(t + PI / 3)<br />Ellipse<br />You provide the orbit for my life.";
+                    break;
+                //抛物线
+                case 3:
+                    n = 2;
+                    Fi = Math.PI / 2;
+                    startLissajousAnimation();
+                    textdiv.style.width = "320px";
+                    changeText();
+                    text2.innerHTML = "x = 200sin(t)<br />y = 200sin(2t + PI / 2)<br />Parabola<br />I start to rise when meeting you.";
+                    break;
+                //执罗诺双纽线
+                case 4:
+                    Fi = 0;
+                    startLissajousAnimation();
+                    textdiv.style.width = "230px";
+                    changeText();
+                    text1.innerHTML = "x = 200sin(t)<br />y = 200sin(2t)<br />Lemniscate<br />Combine together.";
+                    break;
+                //besace曲线
+                case 5:
+                    Fi = Math.PI / 3;
+                    startLissajousAnimation();
+                    textdiv.style.width = "220px";
+                    changeText();
+                    text2.innerHTML = "x = 200sin(t)<br />y = 200sin(2t)<br />Besace<br />Getting beautiful.";
+                    break;
+                //心形线
+                case 6:
+                    startHeartAnimation();
+                    text2.style.display = "none";
+                    textdiv.style.left = "730px";
+                    textdiv.style.top = "190px";
+                    textdiv.style.width = "370px";
+                    textdiv.style.height = "100px";
+                    textdiv.style.boxShadow = "0px 0px 0px #FFF";
+                    text1.style.letterSpacing = "0.5px";
+                    text1.style.textAlign = "center";
+                    text1.style.opacity = "1";
+                    textdiv.style.transition = "all 1.5s linear";
+                    text1.style.transition = "all 2s linear";
+                    text1.innerHTML = "x = 15 * 16[sin(t)]^3<br />y = -16 * (13cost - 5cos2t - 2cos3t - cos4t)<br />Heart<br />I am fond of you.";
+                default:
+                    break;
+            }
+        } else {
+            s += 0.2
+        }
+    }, 10)
 }
 
 function startHeartAnimation() {
-	var s = -80;
-	var b = new Array();
-	var a = setInterval(function() {
-		var h = getHeartCurveCoordinate(s);
-		var e = true;
-		for (var f = 0; f < b.length; f++) {
-			var g = b[f];
-			var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
-			if (j < Garden.options.bloomRadius.max * 1.3) {
-				e = false;
-				break;
-			}
-		}
-		if (e) {
-			b.push(h);
-			garden.createRandomBloom(h[0], h[1])
-		}
-		if (s >= -60) {
-			clearInterval(a);
-			wordsdiv.style.opacity = "1";
-			return;
-		} else {
-			s += 0.2
-		}
-	}, 50)
+    var s = -80;
+    var b = new Array();
+    var a = setInterval(function() {
+        var h = getHeartCurveCoordinate(s);
+        var e = true;
+        for (var f = 0; f < b.length; f++) {
+            var g = b[f];
+            var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
+            if (j < Garden.options.bloomRadius.max * 1.3) {
+                e = false;
+                break;
+            }
+        }
+        if (e) {
+            b.push(h);
+            garden.createRandomBloom(h[0], h[1])
+        }
+        if (s >= -60) {
+            clearInterval(a);
+            wordsdiv.style.opacity = "1";
+           /* wordsdiv.style.boxShadow = "5px 5px 5px gray";*/
+            return;
+        } else {
+            s += 0.2
+        }
+    }, 50)
 }
 
 function clearCanvas() {
-	context.clearRect(0, 0, 600, 600);
-	canvas.width = canvas.width;
+    context.clearRect(0, 0, 600, 600);
+    canvas.width = canvas.width;
 }
 
 //bad code with high CPU consumption.
 /*function sleep(millseconds) {
-	var now = new Date();
-	var exitTime = now.getTime() + millseconds; 
-	while (true) {
-		now = new Date();
-		if (now.getTime() >= exitTime) {
-			return;
-		}
-	}
+    var now = new Date();
+    var exitTime = now.getTime() + millseconds; 
+    while (true) {
+        now = new Date();
+        if (now.getTime() >= exitTime) {
+            return;
+        }
+    }
 }*/
 
 function changeText() {
-	if (text1.style.opacity == "0") {
-		text1.style.opacity = "1";
-		text2.style.opacity = "0";
-	} else if (text1.style.opacity == "1") {
-		text2.style.opacity = "1";
-		text1.style.opacity = "0";
-	}
+    if (text1.style.opacity == "0") {
+        text1.style.opacity = "1";
+        text2.style.opacity = "0";
+    } else if (text1.style.opacity == "1") {
+        text2.style.opacity = "1";
+        text1.style.opacity = "0";
+    }
 }
